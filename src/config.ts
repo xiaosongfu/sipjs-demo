21649d76439162111380825b0056e329407625aa
xiaosongfu <xiaosong.fu@outlook.com>
2019-12-02 18:28:46 +0800

    change fs ip

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,6 @@ export let freeSWITCHServerAddr = '192.168.160.46';
 export let freeSWITCHWebSocketAddr = 'ws://192.168.160.46:5066';
 
 if (process.env.NODE_ENV === 'production') {
-  freeSWITCHServerAddr = '182.61.24.127';
-  freeSWITCHWebSocketAddr = 'wss://182.61.24.127:7443'
+  freeSWITCHServerAddr = '192.168.160.46';
+  freeSWITCHWebSocketAddr = 'wss://192.168.160.46:7443'
 }
