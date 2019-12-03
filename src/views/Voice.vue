5c43b0ddf4525149782c0c55a80b3bac50e962bc
xiaosongfu <xiaosong.fu@outlook.com>
2019-12-03 17:05:55 +0800

    update audio

--- src/views/Voice.vue
+++ src/views/Voice.vue
@@ -1,25 +1,25 @@
 <template>
   <div>
 
-    <div id="login" v-if="showLoginArea">
-      <el-input v-model="account" placeholder="请输入账号"/>
-      <el-input v-model="password" placeholder="请输入密码"/>
+    <div id="login" v-if="loginInfo.showLoginArea">
+      <el-input v-model="loginInfo.account" placeholder="请输入账号"/>
+      <el-input v-model="loginInfo.password" placeholder="请输入密码"/>
       <el-button @click="login" id="loginBtn">登录</el-button>
     </div>
 
-    <div id="funcBtns" v-if="!showLoginArea">
+    <div id="funcBtns" v-if="!loginInfo.showLoginArea">
       <el-button @click="online">签入</el-button>
       <el-button @click="offline">签出</el-button>
       <el-button @click="logout">登出</el-button>
       <hr/>
-      <el-input v-model="destNumber" placeholder="请输入要呼叫的号码"/>
+      <el-input v-model="remoteNumber" placeholder="请输入要呼叫的号码"/>
       <el-button @click="call">拨号</el-button>
 
       <el-dialog
               center
-              :visible.sync="outgoingCalling"
+              :visible.sync="showOutgoingCallDialog"
               width="30%">
-        <span>正在呼叫 {{ destNumber }}</span>
+        <span>正在呼叫 {{ remoteNumber }}</span>
         <span slot="footer" class="dialog-footer">
                 <el-button @click="cancelCall">取消</el-button>
             </span>
@@ -27,9 +27,9 @@
 
       <el-dialog
               center
-              :visible.sync="incomingCalling"
+              :visible.sync="showIncomingCallDialog"
               width="30%">
-        <span>{{ destNumber }} 来话</span>
+        <span>{{ remoteNumber }} 来话</span>
         <span slot="footer" class="dialog-footer">
                 <el-button @click="accept">接受</el-button>
                 <el-button @click="reject">拒绝</el-button>
@@ -38,9 +38,11 @@
 
     </div>
 
-    <div id="video" v-show="showVideo">
-      <video id="localVideo" autoplay muted ref="localVideo"/>
-      <video id="remoteVideo" autoplay ref="remoteVideo"/>
+    <div id="video">
+<!--      <video autoplay muted ref="localVideo"/>-->
+<!--      <video autoplay ref="remoteVideo"/>-->
+      <audio autoplay muted ref="localAudio"/>
+      <audio autoplay ref="remoteAudio"/>
 
       <el-button @click="endCall" id="endCallBtn">挂断</el-button>
     </div>
@@ -56,64 +58,70 @@
     name: 'sip',
     data () {
       return {
-        showVideo: false,
+        loginInfo: {
+          showLoginArea: true,
+          account: '1010',
+          password: '2345',
+        },
 
-        showLoginArea: true,
-        account: '',
-        password: '',
         ua: null,
 
-        destNumber: '',
-        outgoingCalling: false,
         session: null,
-
-        incomingCalling: false,
+        remoteNumber: '1011',
+        showOutgoingCallDialog: false,
+        showIncomingCallDialog: false,
       }
     },
     methods: {
       login () {
         let config = {
-          uri: `${this.account}@${freeSWITCHUserDomain}`,
+          uri: `${this.loginInfo.account}@${freeSWITCHUserDomain}`,
           transportOptions: {
             wsServers: freeSWITCHWebSocketAddr,
             traceSip: true,
           },
-          authorizationUser: this.account,
-          password: this.password,
+          authorizationUser: this.loginInfo.account,
+          password: this.loginInfo.password,
           register: false,
         }
-        // this.ua = new SIP.UA(config)
         this.ua = new UA(config)
 
         // 调用 start() 连接到 WebSocket 服务器，但是不自动调用 register()
         this.ua.start()
 
-        // 注册成功后触发
+        // 注册成功
         this.ua.on('registered', () => {
           this.$message.success('上线成功')
         })
-
+        // 注册失败
         this.ua.on('registrationFailed', (response, cause) => {
           this.$message.error(`注册失败: ${response}`)
           console.error(`注册失败, response: ${response}, cause: ${cause}`)
         })
+        // 下线成功
         this.ua.on('unregistered', (response, cause) => {
-          this.$message.error('下线成功')
-          console.error(`下线成功, response: ${response}, cause: ${cause}`)
-        })
-        this.ua.on('invite', session => {
-          this.session = session
-          this.incomingCalling = true
+          this.$message.success('下线成功')
+          console.log(`下线成功, response: ${response}, cause: ${cause}`)
         })
 
-        this.showLoginArea = false
+        // 通话邀请进来
+        this.ua.on('invite', session => {
+          console.log(session)
+          this.session = session
+          // this.remoteNumber = session.
+          this.showIncomingCallDialog = true
+        })
+
+        // 隐藏登录框
+        this.loginInfo.showLoginArea = false
 
         console.log('login...')
       },
       logout () {
         this.ua.stop()
 
-        this.showLoginArea = true
+        // 显示登录框
+        this.loginInfo.showLoginArea = true
 
         console.log('logout...')
       },
@@ -131,12 +139,12 @@
       // -------------------------
       call () {
         if (this.ua.isRegistered()) {
-          if (!this.destNumber) {
+          if (!this.remoteNumber) {
             this.$message.warning('请输入要呼叫的号码')
             return
           }
 
-          this.session = this.ua.invite(`${this.destNumber}@${freeSWITCHUserDomain}`, {
+          this.session = this.ua.invite(`${this.remoteNumber}@${freeSWITCHUserDomain}`, {
             sessionDescriptionHandlerOptions: {
               constraints: {
                 audio: true,
@@ -145,27 +153,35 @@
             }
           })
 
+          this.showOutgoingCallDialog = true
+
           this.session.on('accepted', (data) => {
             this.$message.success('对方已接受')
             console.log(`对方已接受: ${data}`)
 
-            this.showVideo = true
+            this.showOutgoingCallDialog = false
           })
           this.session.on('rejected', (response, cause) => {
             this.$message.info('对方已拒绝')
             console.log(`对方已拒绝, response: ${response}, cause=${cause}`)
+
+            this.showOutgoingCallDialog = false
           })
 
           this.session.on('failed', (response, cause) => {
             this.$message.error('呼叫失败')
             this.$message.info(`呼叫失败, response: ${response}, cause=${cause}`)
+
+            this.showOutgoingCallDialog = false
           })
 
           this.session.on('bye', (request) => {
             this.$message.info('通话已结束')
             console.log(`通话已结束: ${request}`)
-
-            this.showVideo = false
+          })
+          this.session.on('terminate', (message, cause) => {
+            this.$message.info('通话已结束')
+            console.log(`通话已结束,message : ${message}, cause=${cause}`)
           })
 
           this.session.on('trackAdded', () => {
@@ -176,19 +192,15 @@
             pc.getReceivers().forEach((receiver) => {
               remoteStream.addTrack(receiver.track)
             })
-            this.$refs.remoteVideo.srcObject = remoteStream
-            // this.$refs.remoteVideo.play()
+            this.$refs.remoteAudio.srcObject = remoteStream
 
             // localStream
             let localStream = new MediaStream()
             pc.getSenders().forEach((sender) => {
               localStream.addTrack(sender.track)
             })
-            this.$refs.localVideo.srcObject = localStream
-            // this.$refs.localVideo.play()
+            this.$refs.localAudio.srcObject = localStream
           })
-
-          this.outgoingCalling = true
 
           console.log('invite...')
         } else {
@@ -199,57 +211,54 @@
         this.session.cancel()
         this.$message.info('通话已取消')
 
-        this.outgoingCalling = false
+        this.showOutgoingCallDialog = false
 
         console.log('cancel...')
       },
       endCall () {
-        this.session.terminate()
-        this.$message.info('通话已结束')
+        if (this.session) {
+          this.session.terminate()
+          this.$message.info('通话已结束')
 
-        console.log('terminate...')
+          console.log('terminate...')
+        }
       },
       // -------------------------
       accept () {
         this.session.accept()
         this.$message.info('呼叫已接受')
 
-        this.incomingCalling = false
-
-        this.showVideo = true
+        this.showIncomingCallDialog = false
 
         this.session.on('trackAdded', () => {
           let pc = this.session.sessionDescriptionHandler.peerConnection
 
           // remoteStream
           let remoteStream = new MediaStream()
-          pc.getReceivers().forEach((receiver) => {
+          pc.getReceivers().forEach(receiver => {
+            console.log(receiver)
             remoteStream.addTrack(receiver.track)
           })
-          this.$refs.remoteVideo.srcObject = remoteStream
-          // this.$refs.remoteVideo.play()
+          this.$refs.remoteAudio.srcObject = remoteStream
 
           // localStream
           let localStream = new MediaStream()
-          pc.getSenders().forEach((sender) => {
+          pc.getSenders().forEach(sender => {
             localStream.addTrack(sender.track)
           })
-          this.$refs.localVideo.srcObject = localStream
-          // this.$refs.localVideo.play()
+          this.$refs.localAudio.srcObject = localStream
         })
 
         this.session.on('bye', (request) => {
           this.$message.info('通话已结束')
           console.log(`通话已结束: ${request}`)
-
-          this.showVideo = false
         })
       },
       reject () {
         this.session.reject()
         this.$message.info('呼叫已拒绝')
 
-        this.incomingCalling = false
+        this.showIncomingCallDialog = false
       },
     }
   }
